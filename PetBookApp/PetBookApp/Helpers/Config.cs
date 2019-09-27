6f7d831316d84eaf603995aaed39f99c626e2dbf
gerzonc <gerzonzcanario@gmail.com>
2019-09-27 00:06:53 -0400

    added config (placeholder) and constants to helpers

---
+++ PetBookApp/PetBookApp/Helpers/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetBookApp.Helpers
+{
+    public static class Config
+    {
+        public static string ApiUrl = "";
+        public static string ApiKey = "";
+    }
+}
---
+++ PetBookApp/PetBookApp/Helpers/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetBookApp.Helpers
+{
+    public static class Constants
+    {
+        public static Uri GoToHome = new Uri("HomePage", UriKind.Relative);
+        public static Uri GoToGettingStartedPage = new Uri("GettingStartedPage", UriKind.Relative);
+        public static Uri GoToPostImagePage = new Uri("PostImagePage", UriKind.Relative);
+        public static Uri GoToPostTextPage = new Uri("PostTextPage", UriKind.Relative);
+        public static Uri GoToPostDetailPage = new Uri("PostDetailPage", UriKind.Relative);
+        public static Uri GoToAddPetPage = new Uri("AddPetPage", UriKind.Relative);
+        public static Uri GoToOnBoardingPage = new Uri("OnBoardingPage", UriKind.Relative);
+        public static Uri GoToProfilePage = new Uri("ProfilePage", UriKind.Relative);
+        public static Uri GoToSignInPage = new Uri("SignInPage", UriKind.Relative);
+        public static Uri GoToSignUpPage = new Uri("SignUpPage", UriKind.Relative);
+        public static Uri GoToSignUpPetPage = new Uri("SignUpPetPage", UriKind.Relative);
+        public static Uri GoToFinderPage = new Uri("FinderPage", UriKind.Relative);
+    }
+}
