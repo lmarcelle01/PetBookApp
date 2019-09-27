198a60a9fdee03012f99b285eaaee35f28836ed2
marcosrrg9813 <marcosrrg9813@gmail.com>
2019-09-27 14:20:51 -0400

    added post option to HomePage

--- PetBookApp/PetBookApp/ViewModels/HomePageViewModel.cs
+++ PetBookApp/PetBookApp/ViewModels/HomePageViewModel.cs
@@ -1,8 +1,12 @@
-﻿using Prism.Navigation;
+﻿using PetBookApp.Helpers;
+using Prism.Commands;
+using Prism.Navigation;
 using Prism.Navigation.TabbedPages;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PetBookApp.ViewModels
 {
@@ -11,7 +15,7 @@ namespace PetBookApp.ViewModels
         public event EventHandler IsActiveChanged;
 
         public INavigationService navigationService;
-
+        protected IPageDialogService _dialogService;
 
         private bool _isActive;
         public bool IsActive
@@ -20,17 +24,40 @@ namespace PetBookApp.ViewModels
             set { SetProperty(ref _isActive, value, RaiseIsActiveChanged); }
         }
 
+        public DelegateCommand DisplayPostActionSheetCommand { get; set; }
+
         protected virtual void RaiseIsActiveChanged()
         {
             IsActiveChanged?.Invoke(this, EventArgs.Empty);
         }
-        public HomePageViewModel(INavigationService navigationService) : base(navigationService)
+        public HomePageViewModel(INavigationService navigationService, IPageDialogService dialogService) : base(navigationService)
         {
+            _dialogService = dialogService;
+            DisplayPostActionSheetCommand = new DelegateCommand(async () =>
+            {
+                await DisplayPostActionSheet();
+
+            });
         }
 
         async void SelectTab(object parameters)
         {
             var result = await base.SelectTab("PROFILE");
+        }
+
+        async Task DisplayPostActionSheet()
+        {
+            string action = await _dialogService.DisplayActionSheetAsync("ActionSheet: Select an option", "Cancel", null, "Post text", "Post image");
+            switch (action)
+            {
+                case "Post text":
+                    await NavigateAsync(Constants.GoToPostTextPage);
+                    break;
+                case "Post image":
+                    await NavigateAsync(Constants.GoToPostImagePage);
+                    break;
+
+            }
         }
     }
 }
