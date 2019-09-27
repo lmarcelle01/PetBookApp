6cc2b766cb6c93eec81c558fd6691cda5dc53fac
Lmarcelle01 <lmarcelle01@gmail.com>
2019-09-27 14:54:59 -0400

    Added Navigation

--- PetBookApp/PetBookApp/App.xaml.cs
+++ PetBookApp/PetBookApp/App.xaml.cs
@@ -22,6 +22,8 @@ namespace PetBookApp
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+
             containerRegistry.RegisterForNavigation<HomePage, HomePageViewModel>();
             containerRegistry.RegisterForNavigation<HomeTabbedPage, HomeTabbedPageViewModel>();
             containerRegistry.RegisterForNavigation<SignUpPage, SignUpPageViewModel>();
