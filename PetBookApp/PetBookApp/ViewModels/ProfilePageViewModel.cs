d09d9a0f3fa246e12e708e39d31b186a589755a9
Lmarcelle01 <lmarcelle01@gmail.com>
2019-09-27 11:59:25 -0400

    Fixing Navigation profilePage

--- PetBookApp/PetBookApp/ViewModels/ProfilePageViewModel.cs
+++ PetBookApp/PetBookApp/ViewModels/ProfilePageViewModel.cs
@@ -1,8 +1,11 @@
-﻿using Prism.Navigation;
+﻿using PetBookApp.Helpers;
+using Prism.Commands;
+using Prism.Navigation;
 using Prism.Navigation.TabbedPages;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PetBookApp.ViewModels
 {
@@ -21,9 +24,32 @@ namespace PetBookApp.ViewModels
         {
             IsActiveChanged?.Invoke(this, EventArgs.Empty);
         }
+        public DelegateCommand GoToSettingsCommand { get; set; }
+        public DelegateCommand GoToAddPetCommand { get; set; }
         public ProfilePageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            GoToSettingsCommand = new DelegateCommand(async () =>
+            {
+                await GoToSettingsPage();
 
+            });
+
+            GoToAddPetCommand = new DelegateCommand(async () =>
+            {
+                await GoToAddPetPage();
+
+            });
+
+        }
+
+        async Task GoToSettingsPage()
+        {
+            await NavigateAsync(Constants.GoToSettingsPage);
+        }
+
+        async Task GoToAddPetPage()
+        {
+            await NavigateAsync(Constants.GoToAddPetPage);
         }
 
         async void SelectTab(object parameters)
