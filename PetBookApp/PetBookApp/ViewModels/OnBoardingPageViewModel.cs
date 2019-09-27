79a4aa7531a9c0e862b3028e4e92836511dd34b2
Lmarcelle01 <lmarcelle01@gmail.com>
2019-09-27 10:09:56 -0400

    Added Navigation To OnBoardingPage

--- PetBookApp/PetBookApp/ViewModels/OnBoardingPageViewModel.cs
+++ PetBookApp/PetBookApp/ViewModels/OnBoardingPageViewModel.cs
@@ -1,15 +1,43 @@
-﻿using Prism.Navigation;
+﻿using PetBookApp.Helpers;
+using Prism.Commands;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PetBookApp.ViewModels
 {
     public class OnBoardingPageViewModel : BaseViewModel
     {
+
+        public DelegateCommand GoToSignInPageCommand { get; set; }
+
+        public DelegateCommand GoToSignUpPageCommand { get; set; }
         public OnBoardingPageViewModel(INavigationService navigationService) : base(navigationService)
         {
+            GoToSignInPageCommand = new DelegateCommand(async () =>
+            {
+                await GoToSignIn();
 
+            });
+
+            GoToSignUpPageCommand = new DelegateCommand(async () =>
+            {
+                await GoToSignUp();
+
+            });
         }
+        async Task GoToSignIn()
+        {
+            await NavigateAsync(Constants.GoToSignInPage);
+        }
+
+        async Task GoToSignUp()
+        {
+            await NavigateAsync(Constants.GoToSignUpPage);
+        }
+
+
     }
 }
