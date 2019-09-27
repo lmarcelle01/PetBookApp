1136ca747f0dd606affea9f9b8bb78bc8135250d
gerzonc <gerzonzcanario@gmail.com>
2019-09-27 00:24:39 -0400

    Update Constants.cs

--- PetBookApp/PetBookApp/Helpers/Constants.cs
+++ PetBookApp/PetBookApp/Helpers/Constants.cs
@@ -7,12 +7,12 @@ namespace PetBookApp.Helpers
     public static class Constants
     {
         public static Uri GoToHome = new Uri("HomePage", UriKind.Relative);
-        public static Uri GoToGettingStartedPage = new Uri("GettingStartedPage", UriKind.Relative);
+        public static Uri GoToGettingStartedPage = new Uri("GettingStartedPage", UriKind.Absolute);
         public static Uri GoToPostImagePage = new Uri("PostImagePage", UriKind.Relative);
         public static Uri GoToPostTextPage = new Uri("PostTextPage", UriKind.Relative);
         public static Uri GoToPostDetailPage = new Uri("PostDetailPage", UriKind.Relative);
         public static Uri GoToAddPetPage = new Uri("AddPetPage", UriKind.Relative);
-        public static Uri GoToOnBoardingPage = new Uri("OnBoardingPage", UriKind.Relative);
+        public static Uri GoToOnBoardingPage = new Uri("OnBoardingPage", UriKind.Absolute);
         public static Uri GoToProfilePage = new Uri("ProfilePage", UriKind.Relative);
         public static Uri GoToSignInPage = new Uri("SignInPage", UriKind.Relative);
         public static Uri GoToSignUpPage = new Uri("SignUpPage", UriKind.Relative);
