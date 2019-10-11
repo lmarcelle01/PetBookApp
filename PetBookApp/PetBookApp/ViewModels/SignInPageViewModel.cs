0b871279caf8c5de7b63e16773b8e3f1cf5c4754
marcosrrg9813 <marcosrrg9813@gmail.com>
2019-10-11 13:10:16 -0400

    implemented sign in

--- PetBookApp/PetBookApp/ViewModels/SignInPageViewModel.cs
+++ PetBookApp/PetBookApp/ViewModels/SignInPageViewModel.cs
@@ -1,28 +1,74 @@
 ﻿using PetBookApp.Helpers;
+using PetBookApp.Services;
 using Prism.Commands;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace PetBookApp.ViewModels
 {
     public class SignInPageViewModel : BaseViewModel
     {
 
-        public DelegateCommand GoToHomeTabbedCommand { get; set; }
+
+        public DelegateCommand LogInCommand { get; set; }
+
+        public User User { get; set; }
+
+        public IAuthenticationApi apiService;
         public SignInPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-            GoToHomeTabbedCommand = new DelegateCommand(async () =>
+            User = new User();
+            apiService = new ApiService();
+            LogInCommand = new DelegateCommand(async () =>
             {
-                await GoToHomeTabbed();
+                await LogInAsync();
 
             });
         }
 
-        async Task GoToHomeTabbed()
+        async Task LogInAsync()
         {
+            try
+            {
+                var current = Connectivity.NetworkAccess;
+                if (current == NetworkAccess.Internet)
+                {
+
+                    var userData = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("UserName", User.Email),
+                        new KeyValuePair<string, string>("password", User.Password),
+                        new KeyValuePair<string, string>("grant_type", "password")
+                    };
+
+                    
+                    IsBusy = false;
+                    await apiService.Login(userData);
+                    if(Config.GetToken() != null)
+                    {
+                        await NavigateAsync(Constants.GoToAddPetPage);
+                    }
+                    else
+                    {
+                        await App.Current.MainPage.DisplayAlert("Error", "It seems like the information you gave us is not correct, please try again.", "Ok");
+                    }
+                   
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", "You don't have internet connection", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Unable to connect to the server", "Ok");
+            }
+
+
             await NavigateAsync(Constants.GoToHomeTabbedPage);
         }
 
